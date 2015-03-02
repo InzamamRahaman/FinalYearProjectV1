99640ed7f0c0a60a644001b8c2edffcf3693ea8f
Inzamam <izzyrahaman@gmail.com>
2015-03-02 17:31:23 -0400

    Added some more procedures to the globals

---
+++ List.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:18:04 03/02/2015 
+-- Design Name: 
+-- Module Name:    List - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity List is
+end List;
+
+architecture Behavioral of List is
+
+begin
+
+
+end Behavioral;
+
--- globals.vhd
+++ globals.vhd
@@ -24,7 +24,14 @@ package globals is
 	constant l : integer := 16;  -- the size of the list
 	constant stream_size : integer := 640; -- the size of the code stream
 	subtype int_small is integer range 0 to N; -- a subset of integers for to represent VQ indicies
+	subtype int_index is integer range 0 to l;
 	type index_list is array (0 to l) of int_small; -- an array type for the lists
+	
+	type membership is
+	record
+		present : std_logic;
+		location : int_index;
+	end record;
 	
 --
 -- constant <constant_name>		: time := <time_unit> ns;
@@ -32,7 +39,10 @@ package globals is
 --
 -- Declare functions and procedure
 --	
-	function is_member(xs : index_list; elem : int_small) return std_logic;
+	function is_member(xs : index_list; elem : int_small) return membership;
+	
+	function insert_into_list(xs : index_list; val : int_small; list_size : int_index) return int_index;
+	
 -- function <function_name>  (signal <signal_name> : in <type_declaration>) return <type_declaration>;
 -- procedure <procedure_name> (<type_declaration> <constant_name>	: in <type_declaration>);
 --
@@ -44,16 +54,48 @@ package body globals is
 	-- the function can be used to scan an index list and determine
 	-- if it contains the integer specified
 	function is_member(xs : index_list; elem : int_small)
-	return std_logic is
+	return membership is
+		variable ans : membership;
 	begin
 		for index in 0 to l loop
 			if xs(index) = elem then
-				return '1';
+				ans.present <= '1';
+				ans.location <= index;
+				return ans;
 			end if;
 		end loop; 
-		return '0';
+		ans.present <= '0';
+		return ans;
 	end;
-
+	
+	procedure shift_up(xs : index_list;index : int_index) is
+		
+	begin
+		temp <= xs(index);
+		for idx in index downto 1 loop
+			xs(idx) <= xs(idx - 1);
+		end loop;
+		xs(0) <= temp;
+	end;
+	
+	procedure shift_down(xs : index_list, list_size : int_index) is
+		variable new_size : int_index;
+	begin
+		for idx in (list_size - 1) downto 1 loop
+			xs(idx) <= 
+	end;
+	
+	function insert_into_list(xs : index_list; val : int_small; list_size : int_index)
+	return int_index is
+		variable membership_data : membership;
+	begin
+		membership_data <= is_member(index_list, val);
+		if membership_data.present = '1' then
+			shift_up(xs, membership_data.location);
+		else
+		
+		end if;
+	end;
 ---- Example 1
 --  function <function_name>  (signal <signal_name> : in <type_declaration>  ) return <type_declaration> is
 --    variable <variable_name>     : <type_declaration>;
